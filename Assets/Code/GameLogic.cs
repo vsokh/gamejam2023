c01641d8a5a4d1d9fc999a341c1b1a2632292847
Greenky <valentin.mazurok@gmail.com>
2023-04-08 18:58:32 +0300

    Maze output on screen

--- Assets/Code/GameLogic.cs
+++ Assets/Code/GameLogic.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject NodePrefub;
+	public Vector2 dimentions = new Vector2(6, 6);
+	// Start is called before the first frame update
+	void Start()
+	{
+		RectTransform rt = gameObject.GetComponent<RectTransform>();
+		int w = Mathf.RoundToInt(rt.rect.width);
+		int h = Mathf.RoundToInt(rt.rect.height);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		int gridSize = Mathf.RoundToInt(Mathf.Min(w, h) * 0.9f);
+		float step = gridSize / dimentions.x;
+		Debug.Log(step);
+		Vector2 startCord = new Vector2(-gridSize / 2 + step / 2, gridSize / 2 - step /2);
+		Debug.Log(startCord);
+		for (int i = 0; i < dimentions.x; i++)
+		{
+			for (int j = 0; j < dimentions.y; j++)
+			{
+				Vector2 newCord = startCord + new Vector2(step * i, -step * j);
+				GameObject node = Instantiate(NodePrefub, new Vector3(newCord.x, newCord.y, 0), Quaternion.identity, transform);
+				//node.transform.position -= node.GetComponent<Renderer>().bounds.center;
+				node.GetComponent<RectTransform>().sizeDelta = new Vector2(step, step);
+				node.SetActive(true);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
 }
